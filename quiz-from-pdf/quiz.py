bdf1c6f7dac75aef64f46e40735edf7c862f1559
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-01-02 10:50:20 +0400

    quiz from pdf

--- quiz-from-pdf/quiz.py
+++ quiz-from-pdf/quiz.py
@@ -8,11 +8,15 @@ with open('output_questions.json', 'r') as f:
     while isRunning:
         random_number = random.randint(0, len(questions_data) - 1)
         print("\n" + questions_data[random_number]['question'] + "\n")
+
         for choice in questions_data[random_number]['choices']:
             print("-" + choice[1:].strip())
-        input("\n>>> press enter to see the right answer")
+
+        input("\n>>> press enter to see the right answer\n")
+
         for choice in questions_data[random_number]['choices']:
             print("-" + choice)
-        quit = input("\n>>> press enter to continue or q + enter to stop")
+
+        quit = input("\n>>> press enter to continue or q + enter to stop\n")
         if(quit == "q" or quit == "Q"): isRunning = False
         else: print("\n----------------------------------------------------------------\n")
